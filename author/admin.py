c2181f998ec540e0528651dc4975c458c53497ef
Ruzibek <53888427+RuzibekGit@users.noreply.github.com>
2024-06-26 08:53:28 +0500

    Delete author directory

--- author/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from author.models import AuthorModel
-
-# Register your models here.
-
-
-@admin.register(AuthorModel)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'age', 'email', 'created_at']
