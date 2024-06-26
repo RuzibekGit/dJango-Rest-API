2d434cd42d0709fda4c7754123178b436b2fd4eb
Ruzibek <53888427+RuzibekGit@users.noreply.github.com>
2024-06-26 08:53:41 +0500

    Delete books directory

--- books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from books.models import BooksModel
-
-# Register your models here.
-
-
-@admin.register(BooksModel)
-class BooksAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'subtitle']
