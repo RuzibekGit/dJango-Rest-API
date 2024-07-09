8a23e6a63a1455be9d46c1695cf574984cd4a950
Ruzibek <robidron0199@gmail.com>
2024-07-09 16:15:22 +0500

    *

--- Instagram_Clone/comments/urls.py
+++ Instagram_Clone/comments/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from comments.views import PostCommentListView, PostCommentCreateView
+from comments.views import PostCommentListView, PostCommentCreateView, CommentLikeView
 
 app_name = 'comment'
 
 urlpatterns = [
     path('<int:pk>/', PostCommentListView.as_view(), name='comments-list'),
     path('<int:pk>/create/', PostCommentCreateView.as_view(), name='comments-create'),
+    path('<int:pk>/like/', CommentLikeView.as_view(), name='comments-like'),
 
 ]
--- Instagram_Clone/comments/views.py
+++ Instagram_Clone/comments/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from comments.models import CommentsModel
+
+from comments.models import CommentsModel, CommentLikeModel
 from comments.serializers import  CommentSerializer
 from shared.pagination import CustomPagination
 
@@ -26,3 +29,26 @@ class PostCommentCreateView(generics.CreateAPIView):
         #     serializer.save(parent_id=parent_id)
         post_id = self.kwargs.get('pk')
         serializer.save(user=self.request.user, post_id=post_id)
+
+
+class CommentLikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        comment_like = CommentLikeModel.objects.filter(pk=pk, user=request.user)
+
+        if comment_like.exists():
+            comment_like.delete()
+            response = {
+                "status": True,
+                "message": "Comment unliked"
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            CommentLikeModel.objects.create(user=request.user, post_id=pk)
+            response = {
+                "status": True,
+                "message": "Comment liked"
+            }
+            return Response(response, status=status.HTTP_200_OK)
